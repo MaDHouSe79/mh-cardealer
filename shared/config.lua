df0bcb349b837a36df8d9b72cb4b123fe6a6649d
MaDHouSe <madhouse1979@gmail.com>
2025-11-07 12:45:44 +0100

    Update config.lua

--- shared/config.lua
+++ shared/config.lua
@@ -1,27 +1,27 @@
 ---------------------------------------------------------------------------------------------------
-Config = {}                         -- Placeholder, do not remove or edit this.
-Config.Cardealers = {}              -- Placeholder, do not remove or edit this.
+Config = {}                            -- Placeholder, do not remove or edit this.
+Config.Cardealers = {}                 -- Placeholder, do not remove or edit this.
 ---------------------------------------------------------------------------------------------------
-Config.Framework = "qb"             -- For qb/qbx/esx.
-Config.Target = "qb-target"         -- For qb-target/ox_target
-Config.MoneySign = "€"              -- $ or €
+Config.Framework = "qb"                -- For qb/qbx/esx.
+Config.Target = "qb-target"            -- For qb-target/ox_target
+Config.MoneySign = "€"                 -- $ or €
 ---------------------------------------------------------------------------------------------------
-Config.DiscordLink = ""
+Config.DiscordLink = "https://discord.com/api/webhooks/1436296815992504391/JoTqo55Ea-PnYWBfL4YKauVdrZVIBLuw2cF1RTzIGdku2qjZ60glecotVf6y_pfC3_r7"
 ---------------------------------------------------------------------------------------------------
-Config.UseAsJob = false             -- When try players need to go to the citehall to get the job.
-Config.JobName = 'cardealer2'       -- Job name.
-Config.DaysCardealerExist = 5
-Config.AutomaticDeleteAfterTime = true
+Config.UseAsJob = false                -- Players need to go to the citehall to get the this job.
+Config.JobName = 'cardealer2'          -- Job name.
+Config.DaysCardealerExist = 5          -- amount of days the cardealer exists.
+Config.AutomaticDeleteAfterTime = true -- if true the cardealer automaticly delete after when the time of over.
 ---------------------------------------------------------------------------------------------------
 Config.Display3DText = true
-Config.DisplayInteract = trye      -- When true you see a interact text above the vehicle.
-Config.InteractText = "Eye"         -- 'Eye' for target eye or 'E' for button press.
+Config.DisplayInteract = trye          -- When true you see a interact text above the vehicle.
+Config.InteractText = "Eye"            -- 'Eye' for target eye or 'E' for button press.
 ---------------------------------------------------------------------------------------------------
-Config.SaveCardealersToFile = true  -- When true it save the cardealer to a file, when false the cardealer is only temponary as long as the server is online and not restarting.
-Config.ShowNetIds = false           -- when there is no netid this vehuicle wil nog work, there has to be a netid in order to work.
+Config.SaveCardealersToFile = true     -- When true it save the cardealer to a file, when false the cardealer is only temponary as long as the server is online and not restarting.
+Config.ShowNetIds = false              -- when there is no netid this vehuicle wil nog work, there has to be a netid in order to work.
 Config.ShowPhone = true
 ---------------------------------------------------------------------------------------------------
-Config.DebugPoly = true            -- Debug polyzones.
+Config.DebugPoly = false                -- Debug polyzones.
 ---------------------------------------------------------------------------------------------------
 
 -- Vehicle keys (client side)
